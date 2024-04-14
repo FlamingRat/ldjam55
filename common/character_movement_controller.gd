2b09b0b1885a6d69afadd7b315825abb9f44fa24
fizzydrinks <gabriel@lins.dev>
2024-04-14 01:27:51 -0300

    Animate badgy, fix movement controller flips

--- common/character_movement_controller.gd
+++ common/character_movement_controller.gd
@@ -11,20 +11,18 @@ func move(dir: Vector3) -> void:
 	var parent = get_parent()
 	move_tween.tween_property(parent, "global_position", parent.global_position + dir, 0.3)
 
+	if dir.x < 0 and facing_right:
+		animation_tree.set('parameters/turn_left/request', AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+		facing_right = false
+
+	if dir.x > 0 and not facing_right:
+		animation_tree.set('parameters/turn_right/request', AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+		facing_right = true
+
 
 func move_left() -> void:
 	move(Vector3.LEFT)
-	if not facing_right:
-		return
-	
-	animation_tree.set('parameters/turn_left/request', AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
-	facing_right = false
 
 
 func move_right() -> void:
 	move(Vector3.RIGHT)
-	if facing_right:
-		return
-
-	animation_tree.set('parameters/turn_right/request', AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
-	facing_right = true
--- enemies/badgy.gd
+++ enemies/badgy.gd
@@ -6,6 +6,10 @@ class_name Badgy
 @onready var attack_range := $AttackRange
 @onready var movement := $CharacterMovementController
 @onready var wander := $WanderMovement
+@onready var alignment := $Alignment
+var faction_self: Alignment.Faction:
+	get:
+		return alignment.faction
 
 
 func _on_character_turn_listener_on_turn():
@@ -16,16 +20,18 @@ func _on_character_turn_listener_on_turn():
 
 	for area in sight.get_overlapping_areas():
 		var body = area.get_parent()
-		if body is Badgy:
-			continue
-
-		var dir: Vector3 = (body.global_position - global_position).normalized()
-		var is_x = dir.x > dir.z
-		var mult = -1 if (dir.x if is_x else dir.z) < 0 else 1
-		movement.move((Vector3.RIGHT if is_x else Vector3.BACK) * mult)
-		await get_tree().create_timer(0.4).timeout
-		GlobalEvents.end_turn()
-		return
+		print('detected ', body)
+		var alignment: Alignment = body.get_node('Alignment')
+		if not alignment or alignment.faction != faction_self:
+			var dir: Vector3 = (body.global_position - global_position).normalized()
+			print(dir)
+			var is_x = abs(dir.x) > abs(dir.z)
+			var mult = -1 if (dir.x if is_x else dir.z) < 0 else 1
+			print(is_x, ' ', mult)
+			movement.move((Vector3.RIGHT if is_x else Vector3.BACK) * mult)
+			await get_tree().create_timer(0.4).timeout
+			GlobalEvents.end_turn()
+			return
 
 	wander.rand()
 	GlobalEvents.end_turn()
